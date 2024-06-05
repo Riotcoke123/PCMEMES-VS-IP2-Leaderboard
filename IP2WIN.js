d35b8c53c72b3378d649f2f6ba9a91dba11a88e7
RIOTCOKE <63672863+Riotcoke123@users.noreply.github.com>
2024-06-05 17:48:13 -0500

    Add files via upload

---
+++ IP2WIN.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const urls = [
+    'https://scored.co/api/v2/post/hotv2.json?community=IP2Always',
+    'https://scored.co/api/v2/post/newv2.json?community=IP2Always'
+];
+
+const headers = {
+    'X-API-KEY': '',
+    'X-API-SECRET': '',
+    'Referer': 'https://communities.win/c/IP2Always/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+    'X-Xsrf-Token': '',
+    'X-Api-Platform': 'Scored-Desktop'
+};
+
+async function fetchPosts(url) {
+    try {
+        const response = await axios.get(url, { headers });
+        return response.data.posts;
+    } catch (error) {
+        console.error(`Error fetching data from ${url}:`, error);
+        return [];
+    }
+}
+
+async function main() {
+    const [hotPosts, newPosts] = await Promise.all(urls.map(fetchPosts));
+
+    const allPosts = [...hotPosts, ...newPosts];
+
+    const userMap = new Map();
+
+    allPosts.forEach(item => {
+        const score = item.vote_state * 365;
+        const author = item.author;
+
+        if (!userMap.has(author) || userMap.get(author).score < score) {
+            userMap.set(author, { score, author });
+        }
+    });
+
+    const results = Array.from(userMap.values());
+
+    results.sort((a, b) => b.score - a.score);
+
+    const sortedResults = results.map((result, index) => ({
+        rank: `#${index + 1}`,
+        score: result.score,
+        username: result.author,
+        userLink: `https://communities.win/u/${result.author}`
+    }));
+
+    const outputPath = path.join(__dirname, 'output.json');
+    fs.writeFileSync(outputPath, JSON.stringify(sortedResults, null, 2), 'utf-8');
+    console.log('Data written to output.json');
+}
+
+// Run the main function initially
+main();
+
+// Run the main function every 3 minutes
+setInterval(main, 3 * 60 * 1000);
+
+// API endpoint to serve the results
+app.get('/results', (req, res) => {
+    const outputPath = path.join(__dirname, 'output.json');
+    const data = fs.readFileSync(outputPath, 'utf-8');
+    res.json(JSON.parse(data));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
